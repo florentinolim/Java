c869344e20723c7b1b4730b2fad4bd091b1d770d
Reginaldo <florentinolim@gmail.com>
2022-12-29 19:45:01 -0300

    Novos-exercicios

---
+++ estruturacondicional/src/switchcase/condcase.java
@@ -0,0 +1,42 @@
+package switchcase;
+
+import java.util.Scanner;
+
+public class condcase {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		String dia;
+
+		switch (x) {
+		case 1:
+			dia = "Domingo";
+			break;
+
+		case 2:
+			dia = "Segunda";
+			break;
+		case 3:
+			dia = "Terça";
+			break;
+		case 4:
+			dia = "Quarta";
+			break;
+		case 5:
+			dia = "Quinta";
+		case 6:
+			dia = "Sexta";
+		case 7:
+			dia = "Dia do senhor";
+			break;
+		default:
+			dia = "Dia inexistente";
+		}
+		System.out.println("Dia da semana = \n" + dia);
+		sc.close();
+	}
+
+}
